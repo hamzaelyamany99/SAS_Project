f8565db0efe34dea71cde91bebb1f101a6b39f98
hamzaelyamany <hamza@example.com>
2024-09-10 17:08:09 +0100

    menu

---
+++ Students_Manage.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int i, choice;
+    
+    printf("#####################################################\n");
+    printf("#   1 - Ajouter un étudiant                         #\n");
+    printf("#   2 - Modifier ou supprimer un étudiant           #\n");
+    printf("#   3 - Afficher les détails d'un étudiant          #\n");
+    printf("#   4 - Calculer la moyenne générale                #\n");
+    printf("#   5 - Statistiques                                #\n");
+    printf("#   6 - Rechercher un étudiant par                  #\n");
+    printf("#   7 - Trier un étudiant                           #\n");
+    printf("#####################################################\n");
+
+    scanf("%d",&choice);
+ 
+return 0;
+}
